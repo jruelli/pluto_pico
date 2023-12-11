e099d0ab79e441098082ef699a0568dd8bcc18cd
Jannis Ruellmann <jannis.ruellmann@gmail.com>
2023-12-12 00:09:40 +0100

    add doxygen docu with examples of source code.
    Signed-off-by: Jannis Ruellmann <jannis.ruellmann@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -1,7 +1,18 @@
-/*
- * Copyright (c) 2016 Intel Corporation
+/**
+ * @file main.c
+ * @brief Main entry point for the Pluto_pico project
  *
- * SPDX-License-Identifier: Apache-2.0
+ * This file contains the main function of the Pluto_pico project. It initializes the
+ * system, sets up the USB communication, and starts the LED control thread. This serves
+ * as the primary management function for the application's operation.
+ *
+ * The main function initializes the USB device controller and waits for the Data Terminal
+ * Ready (DTR) signal. Once the DTR signal is received, it initializes the user LED module
+ * and enters a loop that periodically prints "Hello World!" to the console. The LED thread
+ * runs concurrently, toggling the state of an LED at regular intervals.
+ *
+ * @note This application is designed to run on Zephyr RTOS and demonstrates basic usage of
+ *       USB communication and threading with the Zephyr kernel.
  */
 
 #include <zephyr/kernel.h>
@@ -20,6 +31,21 @@ BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
 K_THREAD_STACK_DEFINE(led_stack_area, STACK_SIZE);
 struct k_thread led_thread_data;
 
+/**
+ * @brief Entry point for the Pluto_pico application.
+ *
+ * This is the main function for the Pluto_pico project. It performs the following key tasks:
+ * - Initializes the USB device controller.
+ * - Waits for the Data Terminal Ready (DTR) signal.
+ * - Initializes and starts the user LED control thread.
+ * - Enters a loop that periodically prints "Hello World!" to the console.
+ *
+ * The function sets up a continuous communication channel via USB and initializes the
+ * LED control functionality. The system will continually operate within the main loop,
+ * providing periodic updates to the console and maintaining LED control operations.
+ *
+ * @return int The return value is zero for successful execution and non-zero in case of error.
+ */
 int main(void) {
     const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
     uint32_t dtr = 0;
--- app/src/user_led.c
+++ app/src/user_led.c
@@ -1,11 +1,27 @@
+/**
+ * @file user_led.c
+ * @brief LED control module
+ *
+ * This module provides functions to initialize and control an LED. It includes
+ * both initialization and a continuous control loop for LED state toggling.
+ */
+
 #include "inc/user_led.h"
 #include <zephyr/kernel.h>
 
-/* 1000 msec = 1 sec */
+/* LED toggle interval in milliseconds */
 #define SLEEP_TIME_MS   2000
 
+/* LED device tree specification */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+/**
+ * @brief Initialize the user LED
+ *
+ * This function initializes the user LED specified by the LED device tree
+ * specification. It configures the LED as an output and sets its initial
+ * state to active.
+ */
 void user_led_init(void) {
     if (!gpio_is_ready_dt(&led)) {
         return;
@@ -14,6 +30,13 @@ void user_led_init(void) {
     gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 }
 
+/**
+ * @brief LED control thread
+ *
+ * This thread function toggles the state of the user LED every 2000 milliseconds.
+ * It's designed to run as a continuous thread in a Zephyr application, demonstrating
+ * simple LED control.
+ */
 void user_led_thread(void) {
     while (1) {
         gpio_pin_toggle_dt(&led);
