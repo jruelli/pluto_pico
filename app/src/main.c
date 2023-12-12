8e48547adafd81fbc3dadb6901afc6d049815a10
Jannis Ruellmann <jannis.ruellmann@gmail.com>
2023-12-12 12:23:02 +0100

    lid is being started from usb.
    Signed-off-by: Jannis Ruellmann <jannis.ruellmann@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -28,7 +28,6 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/uart.h>
 
-#include "inc/user_led.h"
 #include "inc/usb_cli.h"
 
 
@@ -37,9 +36,7 @@ BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
 
 /* Stack size and thread data */
 #define STACK_SIZE 1024
-K_THREAD_STACK_DEFINE(led_stack_area, STACK_SIZE);
 K_THREAD_STACK_DEFINE(usb_cli_stack_area, STACK_SIZE);
-struct k_thread led_thread_data;
 struct k_thread usb_cli_thread_data;
 
 /**
@@ -58,14 +55,6 @@ struct k_thread usb_cli_thread_data;
  * @return int The return value is zero for successful execution and non-zero in case of error.
  */
 int main(void) {
-    /* Initialize and start the LED thread */
-    user_led_init();
-    k_tid_t led_tid = k_thread_create(&led_thread_data, led_stack_area,
-                                      K_THREAD_STACK_SIZEOF(led_stack_area),
-                                      (k_thread_entry_t) user_led_thread,
-                                      NULL, NULL, NULL,
-                                      7, 0, K_NO_WAIT);
-
     /* Initialize and start the USB CLI thread */
     usb_cli_init();
     k_tid_t usb_cli_tid = k_thread_create(&usb_cli_thread_data, usb_cli_stack_area,
@@ -73,10 +62,5 @@ int main(void) {
                                           (k_thread_entry_t) usb_cli_thread,
                                           NULL, NULL, NULL,
                                           7, 0, K_NO_WAIT);
-
-    while (1) {
-        k_sleep(K_SECONDS(1));
-    }
-
     return 0;
 }
